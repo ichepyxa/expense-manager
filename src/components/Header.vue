06a51a7d062455a8edbb7796d7dc8f3e889ceb3b
ichepyxa <maxim.krytizna@gmail.com>
2025-07-07 20:21:54 +0300

    add balance, accumulations and operations in dashboard

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,17 +8,17 @@
         </a>
       </div>
       <div class="flex lg:hidden">
-        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
+        <button type="button" class="cursor-pointer -m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 hover:*:stroke-emerald-600 transition-all duration-300" @click="mobileMenuOpen = true">
           <span class="sr-only">Открыть меню</span>
           <Bars3Icon class="size-6" aria-hidden="true" />
         </button>
       </div>
       <div class="hidden lg:flex lg:gap-x-12">
-        <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-sm/6 font-semibold text-gray-900 hover:bg-emerald-600 hover:shadow-lg hover:text-white px-5 py-3 transition-all duration-200 rounded-xl">{{ item.name }}</a>
+        <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-sm/6 font-semibold text-gray-900 hover:bg-emerald-600 hover:shadow-lg hover:text-white px-5 py-3 transition-all duration-300 rounded-xl">{{ item.name }}</a>
       </div>
       <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-        <RouterLink v-if="isAuthenticated" :to="{ name: 'DashboardPage' }" class="text-sm/6 font-semibold text-gray-900 hover:bg-emerald-600 hover:shadow-lg hover:text-white px-5 py-3 transition-all duration-200 rounded-xl">Войти <span aria-hidden="true">&rarr;</span></RouterLink>
-        <RouterLink v-if="!isAuthenticated" :to="{ name: 'LoginPage' }" class="text-sm/6 font-semibold text-gray-900 hover:bg-emerald-600 hover:shadow-lg hover:text-white px-5 py-3 transition-all duration-200 rounded-xl">Войти <span aria-hidden="true">&rarr;</span></RouterLink>
+        <RouterLink v-if="isAuthenticated" :to="{ name: 'DashboardPage' }" class="text-sm/6 font-semibold text-gray-900 hover:bg-emerald-600 hover:shadow-lg hover:text-white px-5 py-3 transition-all duration-300 rounded-xl">Войти <span aria-hidden="true">&rarr;</span></RouterLink>
+        <RouterLink v-if="!isAuthenticated" :to="{ name: 'LoginPage' }" class="text-sm/6 font-semibold text-gray-900 hover:bg-emerald-600 hover:shadow-lg hover:text-white px-5 py-3 transition-all duration-300 rounded-xl">Войти <span aria-hidden="true">&rarr;</span></RouterLink>
       </div>
     </nav>
     <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
@@ -29,7 +29,7 @@
             <span class="sr-only">EMA</span>
             <LogoWithText class="h-20 sm:h-22 w-auto hover:fill-emerald-600 transition-all duration-300" />
           </a>
-          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700 hover:*:stroke-emerald-600 transition-all duration-300 cursor-pointer" @click="mobileMenuOpen = false">
             <span class="sr-only">Закрыть меню</span>
             <XMarkIcon class="size-6" aria-hidden="true" />
           </button>
@@ -37,11 +37,11 @@
         <div class="mt-6 flow-root">
           <div class="-my-6 divide-y divide-gray-500/10">
             <div class="space-y-2 py-6">
-              <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-xl px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-emerald-600 hover:shadow-lg hover:text-white transition-all duration-200">{{ item.name }}</a>
+              <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-xl px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-emerald-600 hover:shadow-lg hover:text-white transition-all duration-300">{{ item.name }}</a>
             </div>
             <div class="py-6">
-              <RouterLink v-if="isAuthenticated" :to="{ name: 'DashboardPage' }" class="-mx-3 block rounded-xl px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-emerald-600 hover:shadow-lg hover:text-white transition-all duration-200">Войти</RouterLink>
-              <RouterLink v-if="!isAuthenticated" :to="{ name: 'LoginPage' }" class="-mx-3 block rounded-xl px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-emerald-600 hover:shadow-lg hover:text-white transition-all duration-200">Войти</RouterLink>
+              <RouterLink v-if="isAuthenticated" :to="{ name: 'DashboardPage' }" class="-mx-3 block rounded-xl px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-emerald-600 hover:shadow-lg hover:text-white transition-all duration-300">Войти</RouterLink>
+              <RouterLink v-if="!isAuthenticated" :to="{ name: 'LoginPage' }" class="-mx-3 block rounded-xl px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-emerald-600 hover:shadow-lg hover:text-white transition-all duration-300">Войти</RouterLink>
             </div>
           </div>
         </div>
