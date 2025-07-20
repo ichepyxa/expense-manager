831512ab453bbc51e44de7f1141a7fb44a428818
ichepyxa <maxim.krytizna@gmail.com>
2025-07-20 11:28:48 +0300

    Add dashboard layout

---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full relative">
+    <Sidebar />
+    <main :class="`${props.mainClasses ?? ''} lg:ml-54 xl:ml-72 p-8 z-10 relative`">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup>
+  import Sidebar from '@/components/Sidebar.vue';
+
+  const props = defineProps({
+    mainClasses: String
+  })
+</script>
